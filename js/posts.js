eb10e6e8031a56370c7f924ed4255fdeb3424f26
Admin <trongphuong005@gmail.com>
2026-03-30 02:47:31 +0700

    update-30-2

--- js/posts.js
+++ js/posts.js
@@ -1,188 +1,289 @@
 const posts = [
+  {
+    title: "Mod Skin FF OB52 V43 Súng M1014 huyết hỏa",
+    desc: "Súng: M1014 trong vòng quay vàng",
+    img: "images/v43_52.jpg",
+    link: "OB52/v43_52.html",
+  },
+  {
+    title: "Mod Skin FF OB52 V42 Set đồ đôi khủng long",
+    desc: "Nhân vật: Ignis + Kapella",
+    img: "images/v42_52.jpg",
+    link: "OB52/v42_52.html",
+  },
+  {
+    title: "Mod Skin FF OB52 V41 Set đồ áo hỏa ấn",
+    desc: "Nhân vật: Ignis + Alok thức tỉnh",
+    img: "images/v41_52.jpg",
+    link: "OB52/v41_52.html",
+  },
+  {
+    title: "Mod Skin FF OB52 V40 Set đồ bá sàn áo gà Piu Piu",
+    desc: "Nhân vật: Alok thức tỉnh",
+    img: "images/v40_52.jpg",
+    link: "OB52/v40_52.html",
+  },
+  {
+    title: "Mod Skin FF OB52 V39 Set đồ Mason",
+    desc: "Nhân vật: Santino",
+    img: "images/v39_52.jpg",
+    link: "OB52/v39_52.html",
+  },
+  {
+    title: "Mod Skin FF OB52 V37 áo pixel nắm đấm đổi màu",
+    desc: "Nhân vật: Ignis + Alok thức tỉnh",
+    img: "images/v37_52.jpg",
+    link: "OB52/v37_52.html",
+  },
 
-{ title:"Mod Skin FF OB52 V35 Set đồ áo Nagi ",
-desc:"Nhân vật: Ignis hoặc Alok thức tỉnh",
-img:"images/v35_52.jpg",
-link:"OB52/v35_52.html" },    
-{ title:"Mod Skin FF OB52 V34 Set đồ hắc tinh  ",
-desc:"Nhân vật: Ignis",
-img:"images/v34_52.jpg",
-link:"OB52/v34_52.html" },    
+  {
+    title: "Mod Skin FF OB52 V36 Hành động M1887 LV7 ",
+    desc: "Hành động: Rét run",
+    img: "images/v36_52.jpg",
+    link: "OB52/v36_52.html",
+  },
 
-{ title:"Mod Skin FF OB52 V33 Set đồ áo sứ giả thần ma ",
-desc:"Nhân vật: Alok thức tỉnh",
-img:"images/v33_52.jpg",
-link:"OB52/v33_52.html" },
+  {
+    title: "Mod Skin FF OB52 V35 Set đồ áo Nagi ",
+    desc: "Nhân vật: Ignis hoặc Alok thức tỉnh",
+    img: "images/v35_52.jpg",
+    link: "OB52/v35_52.html",
+  },
+  {
+    title: "Mod Skin FF OB52 V34 Set đồ hắc tinh  ",
+    desc: "Nhân vật: Ignis",
+    img: "images/v34_52.jpg",
+    link: "OB52/v34_52.html",
+  },
 
-{ title:"Mod Skin FF OB52 V32 Súng MP40 Mãng Xà LV7",
-desc:"Súng: MP40 Phong Xà",
-img:"images/v32_52.jpg",
-link:"OB52/v32_52.html" },  
+  {
+    title: "Mod Skin FF OB52 V33 Set đồ áo sứ giả thần ma ",
+    desc: "Nhân vật: Alok thức tỉnh",
+    img: "images/v33_52.jpg",
+    link: "OB52/v33_52.html",
+  },
 
-{ title:"Mod Skin FF OB52 V31 Set đồ áo Isagi",
-desc:"Nhân vật: Ignis",
-img:"images/v31_52.jpg",
-link:"OB52/v31_52.html" },    
+  {
+    title: "Mod Skin FF OB52 V32 Súng MP40 Mãng Xà LV7",
+    desc: "Súng: MP40 Phong Xà",
+    img: "images/v32_52.jpg",
+    link: "OB52/v32_52.html",
+  },
 
-{ title:"Mod Skin FF OB52 V30 M1887 Hỏa nộ",
-desc:"Súng: M1887 Ánh Sao Thần Thoại hoặc Phong Xà",
-img:"images/v30_52.jpg",
-link:"OB52/v30_52.html" },
+  {
+    title: "Mod Skin FF OB52 V31 Set đồ áo Isagi",
+    desc: "Nhân vật: Ignis",
+    img: "images/v31_52.jpg",
+    link: "OB52/v31_52.html",
+  },
 
+  {
+    title: "Mod Skin FF OB52 V30 M1887 Hỏa nộ",
+    desc: "Súng: M1887 Ánh Sao Thần Thoại hoặc Phong Xà",
+    img: "images/v30_52.jpg",
+    link: "OB52/v30_52.html",
+  },
 
-{ title:"Mod Skin FF OB52 V29 Set đồ áo họa điểu",
-desc:"Nhân vật: ALok thức tỉnh + Balo vợ iu",
-img:"images/v29_52.jpg",
-link:"OB52/v29_52.html" },
+  {
+    title: "Mod Skin FF OB52 V29 Set đồ áo họa điểu",
+    desc: "Nhân vật: ALok thức tỉnh + Balo vợ iu",
+    img: "images/v29_52.jpg",
+    link: "OB52/v29_52.html",
+  },
 
+  {
+    title: "Mod Skin FF OB52 V28 Hành động lv 100",
+    desc: "Hành động: Rét run",
+    img: "images/v28_52.jpg",
+    link: "OB52/v28_52.html",
+  },
 
-{ title:"Mod Skin FF OB52 V28 Hành động lv 100",
-desc:"Hành động: Rét run",
-img:"images/v28_52.jpg",
-link:"OB52/v28_52.html" },
+  {
+    title: "Mod Skin FF OB52 V27 Set Đồ Đôi Bá Sàn",
+    desc: "Nhân vật: Alok và Kapella",
+    img: "images/v27_52.jpg",
+    link: "OB52/v27_52.html",
+  },
 
-{ title:"Mod Skin FF OB52 V27 Set Đồ Đôi Bá Sàn",
-desc:"Nhân vật: Alok và Kapella",
-img:"images/v27_52.jpg",
-link:"OB52/v27_52.html" },
+  {
+    title: "Mod Skin FF OB52 V26 M1887 Vũ Trụ Hủy Diệt",
+    desc: "Súng M1887: Ánh Sao Thần Thoại và Phong Xà",
+    img: "images/v26_52.jpg",
+    link: "OB52/v26_52.html",
+  },
 
+  {
+    title: "Mod Skin FF OB52 V25 Set đồ trai hổ ngáo ",
+    desc: "Nhân vật: Alok",
+    img: "images/v25_52.jpg",
+    link: "OB52/v25_52.html",
+  },
 
-{ title:"Mod Skin FF OB52 V26 M1887 Vũ Trụ Hủy Diệt",
-desc:"Súng M1887: Ánh Sao Thần Thoại và Phong Xà",
-img:"images/v26_52.jpg",
-link:"OB52/v26_52.html" },
+  {
+    title: "Mod Skin FF OB52 V24 Set đồ nhân vật nam nữ",
+    desc: "Nhân vật: Alok và Kapella",
+    img: "images/v24_52.jpg",
+    link: "OB52/v24_52.html",
+  },
 
+  {
+    title: "Mod Skin FF OB52 V23 Set đồ bá sàn map lớn",
+    desc: "Nhân vật: Alok",
+    img: "images/v23_52.jpg",
+    link: "OB52/v23_52.html",
+  },
 
+  {
+    title: "Mod Skin FF OB52 V22 Set đồ áo kim long VIP",
+    desc: "Nhân vật: Alok",
+    img: "images/v22_52.jpg",
+    link: "OB52/v22_52.html",
+  },
 
+  {
+    title: "Mod Skin FF OB52 V21 Set đồ Idol Wasana",
+    desc: "Nhân vật: Santino",
+    img: "images/v21_52.jpg",
+    link: "OB52/v21_52.html",
+  },
 
+  {
+    title: "Mod Skin FF OB52 V20 Balo Vợ Iu thành Long Quyền",
+    desc: "Balo: Vợ iu",
+    img: "images/v20_52.jpg",
+    link: "OB52/v20_52.html",
+  },
 
-{ title:"Mod Skin FF OB52 V25 Set đồ trai hổ ngáo ",
-desc:"Nhân vật: Alok",
-img:"images/v25_52.jpg",
-link:"OB52/v25_52.html" },
+  {
+    title: "Mod Skin FF OB52 V19 Set đồ bá sàn tóc đạo sĩ đỏ",
+    desc: "Nhân vật: Alok + Balo Cá Nóc Đỏ",
+    img: "images/v19_52.jpg",
+    link: "OB52/v19_52.html",
+  },
 
+  {
+    title: "Mod Skin FF OB52 V18 Set đồ bá sàn",
+    desc: "Nhân vật: Ignis + Alok",
+    img: "images/v18_52.jpg",
+    link: "OB52/v18_52.html",
+  },
 
+  {
+    title: "Mod Skin FF OB52 V17 Set đồ đôi bá sàn",
+    desc: "Nhân vật: Alok + Kapella",
+    img: "images/v17_52.jpg",
+    link: "OB52/v17_52.html",
+  },
 
+  {
+    title: "Mod Skin FF OB52 V16 M1887 Vương Quốc Cát",
+    desc: "Súng: M1887 Ánh Sao Thần Thoại",
+    img: "images/v16_52.jpg",
+    link: "OB52/v16_52.html",
+  },
 
-    
-{ title:"Mod Skin FF OB52 V24 Set đồ nhân vật nam nữ",
-desc:"Nhân vật: Alok và Kapella",
-img:"images/v24_52.jpg",
-link:"OB52/v24_52.html" },
+  {
+    title: "Mod Skin FF OB52 V15 Áo Itachi",
+    desc: "Nhân vật: Alok",
+    img: "images/v15_52.jpg",
+    link: "OB52/v15_52.html",
+  },
 
-{ title:"Mod Skin FF OB52 V23 Set đồ bá sàn map lớn",
-desc:"Nhân vật: Alok",
-img:"images/v23_52.jpg",
-link:"OB52/v23_52.html" },
+  {
+    title: "Mod Skin FF OB52 V14 Mèo Kawai",
+    desc: "Nhân vật: Ignis + Alok",
+    img: "images/v14_52.jpg",
+    link: "OB52/v14_52.html",
+  },
 
-{ title:"Mod Skin FF OB52 V22 Set đồ áo kim long VIP",
-desc:"Nhân vật: Alok",
-img:"images/v22_52.jpg",
-link:"OB52/v22_52.html" },
+  {
+    title: "Mod Skin FF OB52 V13 Bá sàn tử chiến",
+    desc: "Nhân vật: Ignis + Alok",
+    img: "images/v13_52.jpg",
+    link: "OB52/v13_52.html",
+  },
 
-{ title:"Mod Skin FF OB52 V21 Set đồ Idol Wasana",
-desc:"Nhân vật: Santino",
-img:"images/v21_52.jpg",
-link:"OB52/v21_52.html" },
+  {
+    title: "Mod Skin FF OB52 V12 Quái nhân hư không",
+    desc: "Nhân vật: Alok + Ignis",
+    img: "images/v12_52.jpg",
+    link: "OB52/v12_52.html",
+  },
 
-{ title:"Mod Skin FF OB52 V20 Balo Vợ Iu thành Long Quyền",
-desc:"Balo: Vợ iu",
-img:"images/v20_52.jpg",
-link:"OB52/v20_52.html" },
+  {
+    title: "Mod Skin FF OB52 V11 Set đồ áo NAGI",
+    desc: "Nhân vật: Alok + Ignis",
+    img: "images/v11_52.jpg",
+    link: "OB52/v11_52.html",
+  },
 
-{ title:"Mod Skin FF OB52 V19 Set đồ bá sàn tóc đạo sĩ đỏ",
-desc:"Nhân vật: Alok + Balo Cá Nóc Đỏ",
-img:"images/v19_52.jpg",
-link:"OB52/v19_52.html" },
+  {
+    title: "Mod Skin FF OB52 V10 Set đồ áo ISAGI có hiệu ứng chạy",
+    desc: "Nhân vật: Alok + Ignis",
+    img: "images/v10_52.jpg",
+    link: "OB52/v10_52.html",
+  },
 
-{ title:"Mod Skin FF OB52 V18 Set đồ bá sàn",
-desc:"Nhân vật: Ignis + Alok",
-img:"images/v18_52.jpg",
-link:"OB52/v18_52.html" },
+  {
+    title: "Mod Skin FF OB52 V9 Set đồ cặp nam nữ siêu VIP",
+    desc: "Nhân vật: Kapella + Ignis",
+    img: "images/v9_52.jpg",
+    link: "OB52/v9_52.html",
+  },
 
-{ title:"Mod Skin FF OB52 V17 Set đồ đôi bá sàn",
-desc:"Nhân vật: Alok + Kapella",
-img:"images/v17_52.jpg",
-link:"OB52/v17_52.html" },
+  {
+    title: "Mod Skin FF OB52 V8 Bá sàn tử chiến",
+    desc: "Nhân vật: Alok + Ignis",
+    img: "images/v8_52.jpg",
+    link: "OB52/v8_52.html",
+  },
 
-{ title:"Mod Skin FF OB52 V16 M1887 Vương Quốc Cát",
-desc:"Súng: M1887 Ánh Sao Thần Thoại",
-img:"images/v16_52.jpg",
-link:"OB52/v16_52.html" },
+  {
+    title: "Mod Skin FF OB52 V7 Áo ngủ bá sàn",
+    desc: "Nhân vật: Alok + Ignis",
+    img: "images/v7_52.jpg",
+    link: "OB52/v7_52.html",
+  },
 
-{ title:"Mod Skin FF OB52 V15 Áo Itachi",
-desc:"Nhân vật: Alok",
-img:"images/v15_52.jpg",
-link:"OB52/v15_52.html" },
+  {
+    title: "Mod Skin FF OB52 V6 - Set đồ cặp PRO",
+    desc: "Nhân vật: Kapella + Ignis",
+    img: "images/v6_52.jpg",
+    link: "OB52/v6_52.html",
+  },
 
-{ title:"Mod Skin FF OB52 V14 Mèo Kawai",
-desc:"Nhân vật: Ignis + Alok",
-img:"images/v14_52.jpg",
-link:"OB52/v14_52.html" },
+  {
+    title: "Mod Skin FF OB52 V5 - M1887 Thánh Phồng",
+    desc: "Súng: M1887 Ánh Sao Thần Thoại",
+    img: "images/v5_52.jpg",
+    link: "OB52/v5_52.html",
+  },
 
-{ title:"Mod Skin FF OB52 V13 Bá sàn tử chiến",
-desc:"Nhân vật: Ignis + Alok",
-img:"images/v13_52.jpg",
-link:"OB52/v13_52.html" },
+  {
+    title: "Mod Skin FF OB52 V4 - Hành Động Scar cá mập đen (lỗi)",
+    desc: "Hành động: Rét run",
+    img: "images/v4_52.jpg",
+    link: "OB52/v4_52.html",
+  },
 
-{ title:"Mod Skin FF OB52 V12 Quái nhân hư không",
-desc:"Nhân vật: Alok + Ignis",
-img:"images/v12_52.jpg",
-link:"OB52/v12_52.html" },
+  {
+    title: "Mod Skin FF OB52 V3",
+    desc: "Nhân vật: Santino",
+    img: "images/v3_52.jpg",
+    link: "OB52/v3_52.html",
+  },
 
-{ title:"Mod Skin FF OB52 V11 Set đồ áo NAGI",
-desc:"Nhân vật: Alok + Ignis",
-img:"images/v11_52.jpg",
-link:"OB52/v11_52.html" },
+  {
+    title: "Mod Skin FF OB52 V2",
+    desc: "Nhân vật: Alok + Ignis",
+    img: "images/v2_52.jpg",
+    link: "OB52/v2_52.html",
+  },
 
-{ title:"Mod Skin FF OB52 V10 Set đồ áo ISAGI có hiệu ứng chạy",
-desc:"Nhân vật: Alok + Ignis",
-img:"images/v10_52.jpg",
-link:"OB52/v10_52.html" },
-
-{ title:"Mod Skin FF OB52 V9 Set đồ cặp nam nữ siêu VIP",
-desc:"Nhân vật: Kapella + Ignis",
-img:"images/v9_52.jpg",
-link:"OB52/v9_52.html" },
-
-{ title:"Mod Skin FF OB52 V8 Bá sàn tử chiến",
-desc:"Nhân vật: Alok + Ignis",
-img:"images/v8_52.jpg",
-link:"OB52/v8_52.html" },
-
-{ title:"Mod Skin FF OB52 V7 Áo ngủ bá sàn",
-desc:"Nhân vật: Alok + Ignis",
-img:"images/v7_52.jpg",
-link:"OB52/v7_52.html" },
-
-{ title:"Mod Skin FF OB52 V6 - Set đồ cặp PRO",
-desc:"Nhân vật: Kapella + Ignis",
-img:"images/v6_52.jpg",
-link:"OB52/v6_52.html" },
-
-{ title:"Mod Skin FF OB52 V5 - M1887 Thánh Phồng",
-desc:"Súng: M1887 Ánh Sao Thần Thoại",
-img:"images/v5_52.jpg",
-link:"OB52/v5_52.html" },
-
-{ title:"Mod Skin FF OB52 V4 - Hành Động Scar cá mập đen (lỗi)",
-desc:"Hành động: Rét run",
-img:"images/v4_52.jpg",
-link:"OB52/v4_52.html" },
-
-{ title:"Mod Skin FF OB52 V3",
-desc:"Nhân vật: Santino",
-img:"images/v3_52.jpg",
-link:"OB52/v3_52.html" },
-
-{ title:"Mod Skin FF OB52 V2",
-desc:"Nhân vật: Alok + Ignis",
-img:"images/v2_52.jpg",
-link:"OB52/v2_52.html" },
-
-{ title:"Mod Skin FF OB52 V1 Mộng Cảnh",
-desc:"Nhân vật: Ignis",
-img:"images/v1_52.jpg",
-link:"OB52/v1_52.html" }
-
+  {
+    title: "Mod Skin FF OB52 V1 Mộng Cảnh",
+    desc: "Nhân vật: Ignis",
+    img: "images/v1_52.jpg",
+    link: "OB52/v1_52.html",
+  },
 ];
